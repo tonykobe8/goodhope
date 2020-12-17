1463cf1508101e2b0b9c2799d03cb2f52ea36e64
tonykobe8 <tonykobe8@gmail.com>
2020-12-17 21:58:06 +0200

    8 files modified

--- src/app/mission/mission.component.ts
+++ src/app/mission/mission.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-mission',
   templateUrl: './mission.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MissionComponent implements OnInit {
 
-  constructor() { }
+ constructor(
+public alertController: AlertController) { }
 
   ngOnInit() {
   }
+async apply() {
+const alert = await this.alertController.create({
+cssClass: 'secondary',
+header:'You Want to Apply?',
+message:'Contact : Mrs Ntshangase | 063 210 7053',
 
+        buttons: [{ text: 'Cancel', role: 'cancel' },
+                  { text: 'ok', handler: (todo) => {
+ 
+          }
+        }
+      ]
+    });
+    alert.present();
+}
 }
--- src/app/objective/objective.component.ts
+++ src/app/objective/objective.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-objective',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjectiveComponent implements OnInit {
 
-  constructor() { }
+   constructor(
+public alertController: AlertController) { }
 
   ngOnInit() {
   }
+async apply() {
+const alert = await this.alertController.create({
+cssClass: 'secondary',
+header:'You Want to Apply?',
+message:'Contact : Mrs Ntshangase | 063 210 7053',
 
+        buttons: [{ text: 'Cancel', role: 'cancel' },
+                  { text: 'ok', handler: (todo) => {
+ 
+          }
+        }
+      ]
+    });
+    alert.present();
+}
 }
